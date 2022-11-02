48a3a1c6206390e796c7d22c1b4e1a4a4751e909
alexey.lazarenko <dzotic9@gmail.com>
2022-11-02 11:08:13 +0200

    JE-63832 Bitrix24 - Create JPS (Clustered)

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -1,1 +1,44 @@
+var nodes = [];
+if ('${settings.ls-addon:false}'== 'true') {
+  nodes.push({
+    nodeType: "litespeedadc",
+    nodeGroup: "bl",
+    cloudlets: 8,
+    count: 2
+  },{
+    nodeType: "litespeedphp",
+    links: "sqldb:DB",
+    nodeGroup: "cp",
+    cloudlets: 16,
+    count: 2
+  });
+} else {
+  nodes.push({
+    nodeType: "nginx",
+    links: "sqldb:DB",
+    nodeGroup: "bl",
+    cloudlets: 16,
+    count: 2
+  }, {
+    nodeType: "nginxphp",
+    links: "sqldb:DB",
+    nodeGroup: "cp",
+    cloudlets: 16,
+    count: 2
+  });
+}
 
+nodes.push({
+  nodeType: "mysql",
+  cloudlets: 10,
+  count: 2
+}, {
+  nodeType: "memcached",
+  cloudlets: 16,
+  count: 2
+});
+
+return {
+  result: 0,
+  nodes: nodes
+}
