ef55704cad6eff935849702cc5ebe88e8957dd39
alexey.lazarenko <dzotic9@gmail.com>
2022-12-06 11:53:39 +0200

    Update dbconn.php

--- scripts/dbconn.php
+++ scripts/dbconn.php
@@ -6,13 +6,13 @@ define("MYSQL_TABLE_TYPE", "INNODB");
 define("BX_UTF", true);
 
 define("DBPersistent", false);
-\$DBType = "mysql";
-\$DBHost = "DB:3306";
-\$DBName = "sitemanager";
-\$DBLogin = "DB_USER";
-\$DBPassword = "DB_PASS";
-\$DBDebug = false;
-\$DBDebugToFile = false;
+$DBType = "mysql";
+$DBHost = "DB:3306";
+$DBName = "sitemanager";
+$DBLogin = "DB_USER";
+$DBPassword = "DB_PASS";
+$DBDebug = false;
+$DBDebugToFile = false;
 
 define("BX_FILE_PERMISSIONS", 0664);
 define("BX_DIR_PERMISSIONS", 0775);
@@ -35,5 +35,5 @@ define("CACHED_b_agent", 3660);
 #define("BX_CACHE_SID", $_SERVER["DOCUMENT_ROOT"]."#01");
 #define("BX_MEMCACHE_HOST", "intIP");
 #define("BX_MEMCACHE_PORT", "11211");
-\$_SERVER["REMOTE_ADDR"] = \$_SERVER['HTTP_CF_CONNECTING_IP'];
+$_SERVER["REMOTE_ADDR"] = \$_SERVER['HTTP_CF_CONNECTING_IP'];
 ?>
